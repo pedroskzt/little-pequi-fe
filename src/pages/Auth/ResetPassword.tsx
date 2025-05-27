347376b0df3eef485cdd8efcd01b4ae535b87906
Pedro Nunes <pedroskzt@gmail.com>
2025-05-26 19:16:14 -0700

    Refactor ResetPassword to add alert and prepare for future backend integration.

--- src/pages/Auth/ResetPassword.tsx
+++ src/pages/Auth/ResetPassword.tsx
@@ -1,4 +1,5 @@
 import {
+    Alert, Collapse,
     Dialog,
     DialogActions,
     DialogContent,
@@ -6,7 +7,7 @@ import {
     DialogTitle,
     TextField
 } from "@mui/material";
-import {FormEvent} from "react";
+import {FormEvent, useState} from "react";
 import LpButton from "../../components/LpButton/LpButton.tsx";
 
 interface IResetPasswordProps {
@@ -15,27 +16,48 @@ interface IResetPasswordProps {
 }
 
 const ResetPassword = ({open, handleClose}: IResetPasswordProps) => {
+    const [openTemp, setOpenTemp] = useState(false);
+    const [email, setEmail] = useState('');
+
+    const closeDialog = () => {
+        setOpenTemp(false);
+        setEmail('');
+        handleClose();
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const payload = {
+            email: email
+        }
+        setOpenTemp(true); // Temporarily shows an alert with a success message
+        console.log(payload);
+        // http.post('/auth/users/reset_password/', payload)
+
+        setTimeout(() => {
+            closeDialog();
+        }, 5000)
+
+    }
     return (
         <>
             <Dialog
                 open={open}
-                onClose={handleClose}
+                onClose={closeDialog}
                 slotProps={{
                     paper: {
                         component: 'form',
-                        onSubmit: (event: FormEvent<HTMLFormElement>) => {
-                            event.preventDefault();
-                            // const formData = new FormData(event.currentTarget);
-                            // const formJson = Object.fromEntries((formData as any).entries());
-                            // const email = formJson.email;
-                            // console.log(email);
-                            handleClose();
-                        }
+                        onSubmit: handleSubmit
                     }
                 }}
 
             >
                 <DialogTitle>Reset Password</DialogTitle>
+                <Collapse in={openTemp}>
+                    <Alert severity="warning">W.I.P.</Alert>
+                    <Alert severity="info" onClose={closeDialog}>An email will be sent to {email} with instructions to
+                        recover your password.</Alert>
+                </Collapse>
                 <DialogContent>
                     <DialogContentText sx={{mb: 1}}>
                         Enter your account&apos;s email address, and we&apos;ll send you a link to
@@ -50,18 +72,23 @@ const ResetPassword = ({open, handleClose}: IResetPasswordProps) => {
                         label="Email address"
                         placeholder="Email address"
                         type="email"
+                        value={email}
+                        onChange={(event) => {
+                            setEmail(event.target.value);
+                        }}
                         fullWidth
                     />
                 </DialogContent>
                 <DialogActions sx={{pb: 3, px: 3}}>
-                    <LpButton onClick={handleClose}>Cancel</LpButton>
+                    <LpButton onClick={closeDialog}>Cancel</LpButton>
                     <LpButton variant="contained" type="submit">
                         Continue
                     </LpButton>
                 </DialogActions>
             </Dialog>
         </>
-    );
+    )
+        ;
 };
 
 export default ResetPassword;
