0c65ba3d94001284962374e9f8f221875422f2c4
Pedro Nunes <pedroskzt@gmail.com>
2025-05-26 19:01:47 -0700

    Apply the authentication context api.

--- src/pages/BasePage.tsx
+++ src/pages/BasePage.tsx
@@ -2,14 +2,18 @@ import LpNavBar from "../components/LpNavBar/LpNavBar.tsx";
 import LpFooter from "../components/LpFooter/LpFooter.tsx";
 import LpHeader from "../components/LpHeader/LpHeader.tsx";
 import {Outlet} from "react-router-dom"
+import {AuthProvider} from "../context/auth/AuthContext.tsx";
 
 
 const BasePage = () => {
+    // const [token, setToken] = useState<string | null>(null);
     return (
         <>
             <LpHeader/>
-            <LpNavBar/>
-            <Outlet/>
+            <AuthProvider>
+                <LpNavBar/>
+                <Outlet/>
+            </AuthProvider>
             <LpFooter/>
         </>
     )
