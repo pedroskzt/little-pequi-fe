65fb4501630719272f92262e9fc0da8825fd71b4
Pedro 'skzt' Nunes <pedroskzt@gmail.com>
2025-07-16 23:12:08 -0700

    Update LpHeader Logo image and responsiveness

--- src/components/LpHeader/LpHeader.tsx
+++ src/components/LpHeader/LpHeader.tsx
@@ -1,10 +1,20 @@
+import {styled} from "@mui/material/styles";
 import logo from "../../assets/logo.png";
-import {Container} from "@mui/material";
+import Container from "@mui/material/Container";
+
+const LogoIcon = styled('img')({
+    width: 'auto',
+    height: 'auto',
+})
 
 const LpHeader = () => {
     return (
         <Container id={"header-logo"} sx={{textAlign: "center", mt: 2}}>
-            <img src={logo} alt="Little Pequi Logo"/>
+            <LogoIcon sx={{
+                maxWidth: {
+                    xs: "50%",
+                    md: "20%",
+                }}} src={logo} alt="Little Pequi Logo"/>
         </Container>
     )
 }
