dc2ef642259f936dd65f2c2fdacaf67b61ab2713
Pedro Nunes <pedroskzt@gmail.com>
2025-10-09 16:43:28 -0700

    Add routes for Admin Panel, restructured BaseUserPage routes for nested navigation.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,20 @@
 import {Route, Routes} from "react-router";
-import Home from '../pages/Home';
 import About from "../pages/About/About.tsx";
+import Admin from "../pages/Admin/Admin.tsx";
+import CategoryCrud from "../pages/Admin/CategoryCrud.tsx";
+import Dashboard from "../pages/Admin/Dashboard.tsx";
+import MenuItemCrud from "../pages/Admin/MenuItemCrud.tsx";
+import TagCrud from "../pages/Admin/TagCrud.tsx";
+import BaseAuthPage from "../pages/Auth/BaseAuthPage.tsx";
 import SignIn from "../pages/Auth/SignIn.tsx";
 import SignUp from "../pages/Auth/SignUp.tsx";
 import BasePage from "../pages/BasePage.tsx";
+import Home from '../pages/Home';
+import MakeOrder from "../pages/MakeOrder/MakeOrder.tsx";
+import RestaurantMenu from "../pages/Menu/Menu.tsx";
+import Reservations from "../pages/Reservations";
 import BaseUserPage from "../pages/User/BaseUserPage.tsx";
 import UserAccount from "../pages/User/UserAccount.tsx";
-import RestaurantMenu from "../pages/Menu/index.tsx";
-import BaseAuthPage from "../pages/Auth/BaseAuthPage.tsx";
-import Reservations from "../pages/Reservations";
-import MakeOrder from "../pages/MakeOrder/MakeOrder.tsx";
 import UserOrder from "../pages/User/UserOrder.tsx";
 
 function LpRoutes() {
@@ -26,9 +31,16 @@ function LpRoutes() {
                 <Route path="/reservations" element={<Reservations/>}/>
                 <Route path="/order" element={<MakeOrder/>}/>
                 <Route path="/user" element={<BaseUserPage/>}>
+                    <Route path="admin" element={<Admin/>}>
+                        <Route index element={<Dashboard/>}/>
+                        <Route path="category" element={<CategoryCrud/>}/>
+                        <Route path="tag" element={<TagCrud/>}/>
+                        <Route path="menu-item" element={<MenuItemCrud/>}/>
+                    </Route>
                     <Route path="account" element={<UserAccount/>}/>
                     <Route path="order" element={<UserOrder/>}/>
                 </Route>
+
             </Route>
         </Routes>
     )
