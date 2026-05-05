f88def63b85dfa3c4f765b68732b70a07708cc2e
Pedro <pedroskzt@gmail.com>
2026-05-04 18:04:02 -0700

    Refactor UserAccount: Replace tokenStore with useAuth for user data handling

--- src/pages/User/UserAccount.tsx
+++ src/pages/User/UserAccount.tsx
@@ -1,30 +1,31 @@
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography";
 import {useEffect} from "react";
-import {useState} from "react";
+// import {useState} from "react";
 import {useNavigate} from "react-router";
-import IUser from "../../interfaces/IUser.ts";
-import {tokenStore} from "../../http/auth.ts";
+// import IUser from "../../interfaces/IUser.ts";
+import {useAuth} from "../../context/auth/AuthContext.ts";
 
 const UserAccount = () => {
 
-    const [userObj, setUserObj] = useState<null | IUser>(null)
-    const user = tokenStore.getUser()
+    // const [userObj, setUserObj] = useState<null | IUser>(null)
+    const {user} = useAuth();
     const navigate = useNavigate();
-
+    //
     useEffect(() => {
         if (user === null) {
             navigate('/')
-        } else {
-            setUserObj(JSON.parse(user))
         }
+        //     } else {
+        //         setUserObj(user)
+        //     }
     }, [user, navigate])
 
 
     return (
         <>
             <Container>
-                <Typography variant={"h1"}>{`${userObj?.first_name} ${userObj?.last_name}`}</Typography>
+                <Typography variant={"h1"}>{`${user?.first_name} ${user?.last_name}`}</Typography>
             </Container>
         </>
     );
