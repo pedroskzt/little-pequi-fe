6b6b751daf4d5080b9889f7747f0b8c009c6d2a4
Pedro <pedroskzt@gmail.com>
2026-05-04 18:03:23 -0700

    Refactor tokenStore: Remove unused USER_KEY and related methods

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -3,13 +3,10 @@ import {jwtDecode} from "jwt-decode";
 export const tokenStore = {
     ACCESS_KEY: "token", //Necessary?
     REFRESH_KEY: "refresh", //Necessary?
-    USER_KEY: "user", //Necessary?
     getAccess: () => sessionStorage.getItem("token"),
     getRefresh: () => sessionStorage.getItem("refresh"),
-    getUser: () => sessionStorage.getItem("user"),
     setAccess: (token: string) => sessionStorage.setItem("token", token),
     setRefresh: (token: string) => sessionStorage.setItem("refresh", token),
-    setUser: (user: string) => sessionStorage.setItem("user", user),
     clear: () => {
         sessionStorage.removeItem("token");
         sessionStorage.removeItem("refresh");
