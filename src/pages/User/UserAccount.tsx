e4010bc902ca980e9e9274a79fa99adac6b78614
Pedro <pedroskzt@gmail.com>
2026-05-09 00:19:33 -0700

    Refactor UserAccount: Remove unused code and simplify component structure

--- src/pages/User/UserAccount.tsx
+++ src/pages/User/UserAccount.tsx
@@ -1,32 +1,18 @@
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography";
-import {useEffect} from "react";
-// import {useState} from "react";
-import {useNavigate} from "react-router";
-// import IUser from "../../interfaces/IUser.ts";
 import {useAuth} from "../../context/auth/AuthContext.ts";
 
 const UserAccount = () => {
 
-    // const [userObj, setUserObj] = useState<null | IUser>(null)
     const {user} = useAuth();
-    const navigate = useNavigate();
-    //
-    useEffect(() => {
-        if (user === null) {
-            navigate('/')
-        }
-        //     } else {
-        //         setUserObj(user)
-        //     }
-    }, [user, navigate])
-
 
     return (
         <>
             <Container>
                 <Typography variant={"h1"}>{`${user?.first_name} ${user?.last_name}`}</Typography>
             </Container>
+
+
         </>
     );
 };
